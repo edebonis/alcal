f66924c06c20de80b4c5adbfa590d6181b6961b1
edebonis@bitson.com.ar <edebonis@bitson.com.ar>
2015-09-21 23:19:37 -0300

    Cambios previos

--- alcal/settings.py
+++ alcal/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -91,3 +92,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
