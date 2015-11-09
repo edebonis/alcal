a0231b07f0e671261561cd9c79b84bc1dc5dec34
edebonis@bitson.com.ar <edebonis@bitson.com.ar>
2015-11-09 17:17:22 -0300

    cambio de nombre notas por calificaciones

--- asistencias/models.py
+++ asistencias/models.py
@@ -6,19 +6,37 @@ from escuela.models import Curso
 from escuela.models import Anio
 
 class CodigoAsistencia(models.Model):
-	codigo = models.CharField(max_length=5)
+	CODIGO_CHOICES = (
+		('A/A','Ausente'),
+		('A/P','A/P'),
+		('A/T','A/T'),
+		('P/A','P/A'),
+		('T/A','T/A'),
+		('P/T','P/T'),
+		('T/P','T/P'),
+		('T/T','T/T'),
+		('R/A','R/A'),
+		('A/R','A/R'),
+		('R/T','R/T'),
+		('T/R','T/R'),
+		('P/R','P/R'),
+		('R/P','R/P'),
+		)
+	codigo = models.CharField(max_length=5, choices=CODIGO_CHOICES, unique=True)
+	cantidad = models.FloatField()
 	def __str__(self):
 		return self.codigo
-	class Meta:
 		verbose_name_plural = "Codigos de Asistencias"
+
 
 class Asistencia(models.Model):
 	ciclo_lectivo = models.ForeignKey(Anio)
-	cantidad = models.IntegerField()
 	curso = models.ForeignKey(Curso)
 	alumno = ChainedForeignKey(Alumno, chained_field="curso", chained_model_field="curso", show_all=False, auto_choose=True)
 	codigo = models.ForeignKey(CodigoAsistencia)
 	fecha = models.DateField()
-	def __str__(self):
-		return str(self.cantidad)
 	
+	def __str__(self):
+		return str(self.fecha)
+	
+
