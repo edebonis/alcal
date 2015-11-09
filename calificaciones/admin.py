987c18a2bc41e5235ad365d8d811adf86fd3df36
edebonis@bitson.com.ar <edebonis@bitson.com.ar>
2015-11-09 17:19:01 -0300

    Agregados nuevos templates

---
+++ calificaciones/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Trimestre, CalificacionTrimestral, CalificacionParcial
+
+
+admin.site.register(Trimestre)
+admin.site.register(CalificacionParcial)
+admin.site.register(CalificacionTrimestral)
